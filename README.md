43c772597894b723ed69d2ce9e9dd97e9b038591
manuel carrascal <tricdiart@gmail.com>
2023-12-19 01:26:54 -0500

    Refactor UI, add README, and comment code

---
+++ README.md
@@ -0,0 +1,26 @@
+# BlenderCollabNotes
+
+BlenderCollabNotes is an addon for Blender that allows you to add notes to the objects in the scene. Each note can have a title, a description, and an importance category. The addon includes an interface to view, edit, and delete notes, and a filter to display notes by category.
+
+## Requirements
+
+- Blender 3.60 or higher
+
+## Installation
+
+1. Download the .zip file from the repository.
+2. Open Blender and go to `Edit > Preferences > Add-ons`.
+3. Click on `Install...` and select the downloaded .zip file.
+4. Activate the addon by checking the box next to `Interface: BlenderCollabNotes`.
+
+## Usage
+
+1. Open the `Tool` panel in the 3D view.
+2. In the `Objects Notes` panel, you can see all existing notes for the objects in the scene.
+3. To add a new note, use the `Create Notes` panel. Enter the title and description of the note, select the object the note refers to, and choose an importance category.
+4. To edit or delete an existing note, use the `Edit Note` and `Delete Note` buttons in the `Objects Notes` panel.
+5. To filter notes by category, use the `Filter by Category` dropdown menu in the `Objects Notes` panel.
+
+## License
+
+This project is licensed under the terms of the Apache 2.0 license.
--- __init__.py
+++ __init__.py
@@ -1,3 +1,4 @@
+# Blender addon information
 bl_info = {
     "name": "BlenderCollabNotes",
     "blender": (3, 60, 0),
@@ -6,7 +7,18 @@ bl_info = {
 
 import bpy
 
+# Function to update object references when the scene is updated
+def update_object_references(dummy):
+    scene = bpy.context.scene
+    for note in scene.object_notes:
+        if note.object_reference in bpy.data.objects:
+            # Update the object reference to the object's current name
+            note.object_reference = bpy.data.objects[note.object_reference].name
 
+# Register the update function as a dependency graph update post-handler
+bpy.app.handlers.depsgraph_update_post.append(update_object_references)
+
+# Custom property group for storing object notes
 class ObjectNoteProperty(bpy.types.PropertyGroup):
     object_reference: bpy.props.StringProperty(
         description="Referenced Object Name"
@@ -25,7 +37,7 @@ class ObjectNoteProperty(bpy.types.PropertyGroup):
         default="No Category"
     )
 
-
+# Custom panel for displaying and editing object notes
 class ObjectNotesPanel(bpy.types.Panel):
     bl_label = "Objects Notes"
     bl_idname = "PT_ObjectNotesPanel"
@@ -49,21 +61,19 @@ class ObjectNotesPanel(bpy.types.Panel):
 
             if i > 0:
                 layout.separator()
+
             box = layout.box()
             row = box.row()
-            row.label(text=f"Object: {note.object_reference}")
 
-            row = box.row()
-            row.label(text="Category:")
-
+            # Display object reference or search field if in edit mode
             if note.is_edit_mode:
-                row.prop(note, "category", text="", emboss=False)
+                row.prop_search(note, "object_reference", bpy.data, "objects", text="Object")
             else:
-                row.label(text=note.category)
+                row.label(text=f"Object: {note.object_reference}")
 
             row = box.row()
 
-            # Editable title
+            # Display title or editable title field if in edit mode
             if note.is_edit_mode:
                 row.prop(note, "note_title", text="Title")
             else:
@@ -71,20 +81,30 @@ class ObjectNotesPanel(bpy.types.Panel):
 
             row = box.row()
 
-            # Editable description
+            # Display description or editable description field if in edit mode
             if note.is_edit_mode:
                 row.prop(note, "note_description", text="Description")
             else:
                 row.label(text=f"Description: {note.note_description}")
 
             row = box.row()
+
+            # Display category or editable category field if in edit mode
+            if note.is_edit_mode:
+                row.prop(note, "category", text="Category", emboss=False)
+            else:
+                row.label(text=f"Category: {note.category}")
+
+            row = box.row()
+
+            # Display buttons for editing or deleting notes
             if note.is_edit_mode:
                 row.operator("object.edit_object_note", text="Finish Edit").note_index = i
             else:
                 row.operator("object.edit_object_note", text="Edit Note").note_index = i
                 row.operator("object.remove_object_note", text="Delete Note").note_index = i
 
-
+# Custom panel for creating new object notes
 class AddObjectNotePanel(bpy.types.Panel):
     bl_label = "Create Notes"
     bl_idname = "PT_AddObjectNotePanel"
@@ -107,18 +127,24 @@ class AddObjectNotePanel(bpy.types.Panel):
         row.prop(scene.object_notes_temp, "note_description", text="")
 
         row = layout.row()
+
+        # Display search field for object reference
+        row.prop_search(scene.object_notes_temp, "object_reference", bpy.data, "objects", text="Object")
+
+        row = layout.row()
         row.label(text="Category:")
 
-        # Modifica esta línea para evitar que "All" esté disponible al crear una nueva nota
+        # Display category field if not in edit mode
         if not scene.object_notes_temp.is_edit_mode:
             row.prop(scene.object_notes_temp, "category", text="")
 
         row = layout.row()
+        # Disable the add note button if title or description is empty
         if not scene.object_notes_temp.note_title or not scene.object_notes_temp.note_description:
             row.enabled = False
         row.operator("object.add_object_note", text="Add Note")
 
-
+# Custom operator for adding a new object note
 class AddObjectNoteOperator(bpy.types.Operator):
     bl_idname = "object.add_object_note"
     bl_label = "Add Note to Object"
@@ -129,18 +155,20 @@ class AddObjectNoteOperator(bpy.types.Operator):
         selected_obj = context.active_object
 
         if selected_obj:
+            # Create a new note with the selected object's information
             note = scene.object_notes.add()
             note.object_reference = selected_obj.name
             note.note_title = scene.object_notes_temp.note_title
             note.note_description = scene.object_notes_temp.note_description
             note.category = scene.object_notes_temp.category
 
+        # Reset temporary note data
         scene.object_notes_temp.note_title = ""
         scene.object_notes_temp.note_description = ""
 
         return {'FINISHED'}
 
-
+# Custom operator for editing an object note
 class EditObjectNoteOperator(bpy.types.Operator):
     bl_idname = "object.edit_object_note"
     bl_label = "Edit Object Note"
@@ -152,11 +180,12 @@ class EditObjectNoteOperator(bpy.types.Operator):
         scene = context.scene
         note = scene.object_notes[self.note_index]
 
+        # Toggle edit mode
         note.is_edit_mode = not note.is_edit_mode
 
         return {'FINISHED'}
 
-
+# Custom operator for removing an object note
 class RemoveObjectNoteOperator(bpy.types.Operator):
     bl_idname = "object.remove_object_note"
     bl_label = "Delete Object Note"
@@ -166,11 +195,12 @@ class RemoveObjectNoteOperator(bpy.types.Operator):
 
     def execute(self, context):
         scene = context.scene
+        # Remove the selected object note
         scene.object_notes.remove(self.note_index)
 
         return {'FINISHED'}
 
-
+# Register the classes and properties
 def register():
     bpy.utils.register_class(ObjectNoteProperty)
     bpy.utils.register_class(ObjectNotesPanel)
@@ -193,7 +223,7 @@ def register():
         default="All"
     )
 
-
+# Unregister the classes and properties
 def unregister():
     bpy.utils.unregister_class(ObjectNoteProperty)
     bpy.utils.unregister_class(ObjectNotesPanel)
@@ -202,10 +232,11 @@ def unregister():
     bpy.utils.unregister_class(RemoveObjectNoteOperator)
     bpy.utils.unregister_class(AddObjectNotePanel)
 
+    # Remove custom properties
     del bpy.types.Scene.object_notes
     del bpy.types.Scene.object_notes_temp
     del bpy.types.Scene.object_notes_category_filter
 
-
+# Run the registration function if the script is executed directly
 if __name__ == "__main__":
     register()
